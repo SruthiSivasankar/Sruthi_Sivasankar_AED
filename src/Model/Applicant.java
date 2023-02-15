b3dda9b65487b3e67aca8569cde1029f13236799
SruthiSivasankar <sivasankar.s@northeastern.edu>
2023-02-15 00:22:03 -0500

    initial commit

---
+++ src/Model/Applicant.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author sruthisivasankar
+ */
+public class Applicant {
+    String applicationId;
+    String OwnerFirstName;
+    String ownerLastName;
+    String applicationDate;
+    
+}
---
+++ src/Model/Pet.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author sruthisivasankar
+ */
+public class Pet {
+    String petName;
+    int petAge;
+    String petGender;
+    String petType;
+    String petbreed;
+    
+}
